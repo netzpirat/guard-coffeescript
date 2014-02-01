a3faf5456f93e8afdef10786d3a2e1152e15faea
Oliver Chang <oliver@oychang.com>
2014-02-01 13:27:13 -0500

    Update runner_spec to include literate tests.
    Updated old tests to include both new extensions generally, and also add
    a new test to ensure the literate option is set.

--- spec/guard/coffeescript/runner_spec.rb
+++ spec/guard/coffeescript/runner_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Guard::CoffeeScript::Runner do
   let(:runner) { Guard::CoffeeScript::Runner }
-  let(:watcher) { Guard::Watcher.new('^(.*)\.coffee') }
+  let(:watcher) { Guard::Watcher.new('^(.+)\.(?:coffee|coffee\.md|litcoffee)$') }
   let(:formatter) { Guard::CoffeeScript::Formatter }
 
   before do
@@ -17,48 +17,56 @@ describe Guard::CoffeeScript::Runner do
   describe '#run' do
     context 'without the :noop option' do
       it 'shows a start notification' do
-        formatter.should_receive(:info).once.with('Compile a.coffee, b.coffee', { :reset => true })
+        formatter.should_receive(:info).once.with('Compile a.coffee, b.coffee.md, c.litcoffee', { :reset => true })
         formatter.should_receive(:success).once.with('Successfully generated ')
-        runner.run(['a.coffee', 'b.coffee'], [])
+        runner.run(['a.coffee', 'b.coffee.md', 'c.litcoffee'], [])
       end
     end
 
     context 'with the :noop option' do
       it 'shows a start notification' do
-        formatter.should_receive(:info).once.with('Verify a.coffee, b.coffee', { :reset => true })
+        formatter.should_receive(:info).once.with('Verify a.coffee, b.coffee.md, c.litcoffee', { :reset => true })
         formatter.should_receive(:success).once.with('Successfully verified ')
-        runner.run(['a.coffee', 'b.coffee'], [], { :noop => true })
+        runner.run(['a.coffee', 'b.coffee.md', 'c.litcoffee'], [], { :noop => true })
       end
     end
 
     context 'without a nested directory' do
-      let(:watcher) { Guard::Watcher.new(%r{src/.+\.coffee}) }
+      let(:watcher) { Guard::Watcher.new(%r{src/.+\.(?:coffee|coffee\.md|litcoffee)$}) }
 
       context 'without the :noop option' do
-        it 'compiles the CoffeeScripts to the output and replace .coffee with .js' do
+        it 'compiles the CoffeeScripts to the output and replace .{coffee,coffee.md,litcoffee} with .js' do
           FileUtils.should_receive(:mkdir_p).with("#{ @project_path }/target")
           File.should_receive(:open).with("#{ @project_path }/target/a.js", 'w')
-          runner.run(['src/a.coffee'], [watcher], { :output => 'target' })
+          File.should_receive(:open).with("#{ @project_path }/target/b.js", 'w')
+          File.should_receive(:open).with("#{ @project_path }/target/c.js", 'w')
+          runner.run(['src/a.coffee', 'src/b.coffee.md', 'src/c.litcoffee'], [watcher], { :output => 'target' })
         end
 
-        it 'compiles the CoffeeScripts to the output and replace .js.coffee with .js' do
+        it 'compiles the CoffeeScripts to the output and replace .js.{coffee,coffee.md,litcoffee} with .js' do
           FileUtils.should_receive(:mkdir_p).with("#{ @project_path }/target")
           File.should_receive(:open).with("#{ @project_path }/target/a.js", 'w')
-          runner.run(['src/a.js.coffee'], [watcher], { :output => 'target' })
+          File.should_receive(:open).with("#{ @project_path }/target/b.js", 'w')
+          File.should_receive(:open).with("#{ @project_path }/target/c.js", 'w')
+          runner.run(['src/a.js.coffee', 'src/b.js.coffee.md', 'src/c.litcoffee'], [watcher], { :output => 'target' })
         end
       end
 
       context 'without the :output option' do
-        it 'compiles the CoffeeScripts to the same dir like the file and replace .coffee with .js' do
+        it 'compiles the CoffeeScripts to the same dir like the file and replace .{coffee,coffee.md,litcoffee} with .js' do
           FileUtils.should_receive(:mkdir_p).with("#{ @project_path }/src")
           File.should_receive(:open).with("#{ @project_path }/src/a.js", 'w')
-          runner.run(['src/a.coffee'], [watcher])
+          File.should_receive(:open).with("#{ @project_path }/src/b.js", 'w')
+          File.should_receive(:open).with("#{ @project_path }/src/c.js", 'w')
+          runner.run(['src/a.coffee', 'src/b.coffee.md', 'src/c.litcoffee'], [watcher])
         end
 
-        it 'compiles the CoffeeScripts to the same dir like the file and replace .js.coffee with .js' do
+        it 'compiles the CoffeeScripts to the same dir like the file and replace .js.{coffee,coffee.md,litcoffee} with .js' do
           FileUtils.should_receive(:mkdir_p).with("#{ @project_path }/src")
           File.should_receive(:open).with("#{ @project_path }/src/a.js", 'w')
-          runner.run(['src/a.js.coffee'], [watcher])
+          File.should_receive(:open).with("#{ @project_path }/src/b.js", 'w')
+          File.should_receive(:open).with("#{ @project_path }/src/c.js", 'w')
+          runner.run(['src/a.js.coffee', 'src/b.js.coffee.md', 'src/c.js.litcoffee'], [watcher])
         end
       end
 
@@ -66,28 +74,34 @@ describe Guard::CoffeeScript::Runner do
         it 'does not write the output file' do
           FileUtils.should_not_receive(:mkdir_p).with("#{ @project_path }/target")
           File.should_not_receive(:open).with("#{ @project_path }/target/a.js", 'w')
-          runner.run(['src/a.coffee'], [watcher], { :output => 'target', :noop => true })
+          File.should_not_receive(:open).with("#{ @project_path }/target/b.js", 'w')
+          File.should_not_receive(:open).with("#{ @project_path }/target/c.js", 'w')
+          runner.run(['src/a.js.coffee', 'src/b.js.coffee.md', 'src/c.js.litcoffee'], [watcher], { :output => 'target', :noop => true })
         end
       end
 
       context 'with the :source_map option' do
-        it 'compiles the source map to the same dir like the file and replace .coffee with .js.map' do
+        it 'compiles the source map to the same dir like the file and replace .{coffee,coffee.md,litcoffee} with .js.map' do
           FileUtils.should_receive(:mkdir_p).with("#{ @project_path }/src")
           File.should_receive(:open).with("#{ @project_path }/src/a.js.map", 'w')
-          runner.run(['src/a.coffee'], [watcher], :source_map => true)
+          File.should_receive(:open).with("#{ @project_path }/src/b.js.map", 'w')
+          File.should_receive(:open).with("#{ @project_path }/src/c.js.map", 'w')
+          runner.run(['src/a.coffee', 'src/b.coffee.md', 'src/c.litcoffee'], [watcher], :source_map => true)
         end
 
-        it 'compiles the source map to the same dir like the file and replace .js.coffee with .js.map' do
+        it 'compiles the source map to the same dir like the file and replace .js.{coffee,coffee.md,litcoffee} with .js.map' do
           FileUtils.should_receive(:mkdir_p).with("#{ @project_path }/src")
           File.should_receive(:open).with("#{ @project_path }/src/a.js.map", 'w')
-          runner.run(['src/a.js.coffee'], [watcher], :source_map => true)
+          File.should_receive(:open).with("#{ @project_path }/src/b.js.map", 'w')
+          File.should_receive(:open).with("#{ @project_path }/src/c.js.map", 'w')
+          runner.run(['src/a.js.coffee', 'src/b.js.coffee.md', 'src/c.js.litcoffee'], [watcher], :source_map => true)
         end
 
       end
     end
 
     context 'with the :bare option set to an array of filenames' do
-      let(:watcher) { Guard::Watcher.new(%r{src/.+\.coffee}) }
+      let(:watcher) { Guard::Watcher.new(%r{src/.+\.(?:coffee|coffee\.md|litcoffee)$}) }
 
       before do
         runner.unstub(:compile)
@@ -108,37 +122,49 @@ describe Guard::CoffeeScript::Runner do
     end
 
     context 'with the :shallow option set to false' do
-      let(:watcher) { Guard::Watcher.new('^app/coffeescripts/(.*)\.coffee') }
+      let(:watcher) { Guard::Watcher.new('^app/coffeescripts/(.+)\.(?:coffee|coffee\.md|litcoffee)$') }
 
       it 'compiles the CoffeeScripts to the output and creates nested directories' do
         FileUtils.should_receive(:mkdir_p).with("#{ @project_path }/javascripts/x/y")
         File.should_receive(:open).with("#{ @project_path }/javascripts/x/y/a.js", 'w')
-        runner.run(['app/coffeescripts/x/y/a.coffee'], [watcher], { :output => 'javascripts', :shallow => false })
+        File.should_receive(:open).with("#{ @project_path }/javascripts/x/y/b.js", 'w')
+        File.should_receive(:open).with("#{ @project_path }/javascripts/x/y/c.js", 'w')
+        runner.run(['app/coffeescripts/x/y/a.coffee', 'app/coffeescripts/x/y/b.coffee.md', 'app/coffeescripts/x/y/c.litcoffee'],
+                   [watcher], { :output => 'javascripts', :shallow => false })
       end
 
       context 'with the :source_map option' do
         it 'generates the source map to the output and creates nested directories' do
           FileUtils.should_receive(:mkdir_p).with("#{ @project_path }/javascripts/x/y")
           File.should_receive(:open).with("#{ @project_path }/javascripts/x/y/a.js.map", 'w')
-          runner.run(['app/coffeescripts/x/y/a.coffee'], [watcher], { :output => 'javascripts', :shallow => false, :source_map => true })
+          File.should_receive(:open).with("#{ @project_path }/javascripts/x/y/b.js.map", 'w')
+          File.should_receive(:open).with("#{ @project_path }/javascripts/x/y/c.js.map", 'w')
+          runner.run(['app/coffeescripts/x/y/a.coffee', 'app/coffeescripts/x/y/b.coffee.md', 'app/coffeescripts/x/y/c.litcoffee'],
+                     [watcher], { :output => 'javascripts', :shallow => false, :source_map => true })
         end
       end
     end
 
     context 'with the :shallow option set to true' do
-      let(:watcher) { Guard::Watcher.new('^app/coffeescripts/(.*)\.coffee') }
+      let(:watcher) { Guard::Watcher.new('^app/coffeescripts/(.+)\.(?:coffee|coffee\.md|litcoffee)$') }
 
       it 'compiles the CoffeeScripts to the output without creating nested directories' do
         FileUtils.should_receive(:mkdir_p).with("#{ @project_path }/javascripts")
         File.should_receive(:open).with("#{ @project_path }/javascripts/a.js", 'w')
-        runner.run(['app/coffeescripts/x/y/a.coffee'], [watcher], { :output => 'javascripts', :shallow => true })
+        File.should_receive(:open).with("#{ @project_path }/javascripts/b.js", 'w')
+        File.should_receive(:open).with("#{ @project_path }/javascripts/c.js", 'w')
+        runner.run(['app/coffeescripts/x/y/a.coffee', 'app/coffeescripts/x/y/b.coffee.md', 'app/coffeescripts/x/y/c.litcoffee'],
+                   [watcher], { :output => 'javascripts', :shallow => true })
       end
 
       context 'with the :source_map option' do
         it 'generates the source map to the output without creating nested directories' do
           FileUtils.should_receive(:mkdir_p).with("#{ @project_path }/javascripts")
           File.should_receive(:open).with("#{ @project_path }/javascripts/a.js.map", 'w')
-          runner.run(['app/coffeescripts/x/y/a.coffee'], [watcher], { :output => 'javascripts', :shallow => true, :source_map => true })
+          File.should_receive(:open).with("#{ @project_path }/javascripts/b.js.map", 'w')
+          File.should_receive(:open).with("#{ @project_path }/javascripts/c.js.map", 'w')
+          runner.run(['app/coffeescripts/x/y/a.coffee', 'app/coffeescripts/x/y/b.coffee.md', 'app/coffeescripts/x/y/c.litcoffee'],
+                     [watcher], { :output => 'javascripts', :shallow => true, :source_map => true })
         end
       end
     end
@@ -168,6 +194,26 @@ describe Guard::CoffeeScript::Runner do
       it 'accepts a different source_root' do
         ::CoffeeScript.should_receive(:compile).with 'src/a.coffee', hash_including(:sourceRoot => 'foo')
         runner.run(['src/a.coffee'], [watcher], { :output => 'target', :source_map => true, :source_root => 'foo' })
+      end
+    end
+
+    context 'with literate coffeescript' do
+      before do
+        runner.unstub(:compile)
+        ::CoffeeScript.stub(:compile)
+        File.stub(:read) { |file| file }
+      end
+
+      after do
+        runner.stub(:compile).and_return ''
+        ::CoffeeScript.unstub(:compile)
+      end
+
+      it 'compiles with the :literate option set' do
+        ::CoffeeScript.should_receive(:compile).with 'a.coffee', hash_not_including(:literate => true)
+        ::CoffeeScript.should_receive(:compile).with 'b.coffee.md', hash_including(:literate => true)
+        ::CoffeeScript.should_receive(:compile).with 'c.litcoffee', hash_including(:literate => true)
+        runner.run(['a.coffee', 'b.coffee.md', 'c.litcoffee'], [watcher], { :output => 'javascripts' })
       end
     end
 
@@ -227,7 +273,7 @@ describe Guard::CoffeeScript::Runner do
       end
 
       context 'with the :hide_success option set to true' do
-        let(:watcher) { Guard::Watcher.new('^app/coffeescripts/(.*)\.coffee') }
+        let(:watcher) { Guard::Watcher.new('^app/coffeescripts/.+\.(?:coffee|coffee\.md|litcoffee)$') }
 
         it 'does not show the success message' do
           formatter.should_not_receive(:success).with('Successfully generated javascripts/a.js')
@@ -268,25 +314,27 @@ describe Guard::CoffeeScript::Runner do
   end
 
   describe '#remove' do
-    let(:watcher) { Guard::Watcher.new(%r{src/.+\.coffee}) }
+    let(:watcher) { Guard::Watcher.new(%r{src/.+\.(?:coffee|coffee\.md|litcoffee)$}) }
 
     before do
       File.should_receive(:exists?).with('target/a.js').and_return true
       File.should_receive(:exists?).with('target/b.js').and_return true
+      File.should_receive(:exists?).with('target/c.js').and_return true
     end
 
     it 'removes the files' do
       FileUtils.should_receive(:remove_file).with('target/a.js')
       FileUtils.should_receive(:remove_file).with('target/b.js')
-      runner.remove(['src/a.coffee', 'src/b.coffee'], [watcher], { :output => 'target' })
+      FileUtils.should_receive(:remove_file).with('target/c.js')
+      runner.remove(['src/a.coffee', 'src/b.coffee.md', 'src/c.litcoffee'], [watcher], { :output => 'target' })
     end
 
     it 'shows a notification' do
-      formatter.should_receive(:success).once.with('Removed target/a.js, target/b.js')
-      formatter.should_receive(:notify).with('Removed target/a.js, target/b.js',
+      formatter.should_receive(:success).once.with('Removed target/a.js, target/b.js, target/c.js')
+      formatter.should_receive(:notify).with('Removed target/a.js, target/b.js, target/c.js',
                                              :title => 'CoffeeScript results')
 
-      runner.remove(['src/a.coffee', 'src/b.coffee'], [watcher], { :output => 'target' })
+      runner.remove(['src/a.coffee', 'src/b.coffee.md', 'src/c.litcoffee'], [watcher], { :output => 'target' })
     end
   end
 end
